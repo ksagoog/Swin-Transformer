fcdb1fbc26fcca9584b13e0a3b0b68ff2847627b
Kyle <kylesargent@google.com>
2022-03-15 01:01:22 +0000

    Modifications

--- launch_8gpu.sh
+++ launch_8gpu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m torch.distributed.launch --nproc_per_node 8 --master_port 12345  main.py --cfg configs/swin_base_patch4_window7_224.yaml --zip --data-path "${INPUT_DIR}" --cache-mode ${CACHE_MODE} --accumulation-steps 1 --batch-size 64 --pyramid_adversarial_training=${PYRAMID_ADVERSARIAL_TRAINING} --output_dir="${OUTPUT_DIR}"
+python3 -m torch.distributed.launch --nproc_per_node 8 --master_port 12345  main.py --cfg configs/swin_base_patch4_window7_224.yaml --zip --data-path "${INPUT_DIR}" --cache-mode ${CACHE_MODE} --accumulation-steps 2 --batch-size 64 --pyramid_adversarial_training=${PYRAMID_ADVERSARIAL_TRAINING} --output_dir="${OUTPUT_DIR}"
---
+++ set_envvar.sh
@@ -0,0 +1,4 @@
+export INPUT_DIR="/home/jupyter/ImageNet-Zip"
+export CACHE_MODE="part"
+export PYRAMID_ADVERSARIAL_TRAINING="no"
+export OUTPUT_DIR="output/"
